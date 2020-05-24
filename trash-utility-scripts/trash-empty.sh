260040ac346a079672d9051257e85d20fe73bf85
Atharva Kadam <atharva@linux-course.us-central1-a.c.tough-anvil-251404.internal>
2020-05-24 09:26:24 +0000

    Fixed the .trashcan edit for trash-empty

--- trash-utility-scripts/trash-empty.sh
+++ trash-utility-scripts/trash-empty.sh
@@ -4,7 +4,7 @@
 # Custom script to empty the trashcan #
 #######################################
 
-if [ -d "/trashcan" ]
+if [ -d "/home/${USER}/.trashcan" ]
 then
   read -p "Are you sure you want to empty your trash can?(Please enter y/n): " user_input
   echo $user_input
@@ -17,11 +17,11 @@ then
   done
   if [ "$user_input" = "y" ]; then
     echo "Okay!. Lets empty your trash can."
-    files_to_del=`ls /trashcan`
+    files_to_del=`ls /home/${USER}/.trashcan`
     for files in $files_to_del
     do
       echo "${files} deleted"
-      sudo rm -rf "/trashcan/$files" 
+      rm -rf "/home/${USER}/.trashcan/$files"
     done
     echo "Trash can emptied.All files deleted permanently."
   else
